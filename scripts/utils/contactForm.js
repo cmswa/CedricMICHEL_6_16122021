c04793be9de465f88d9ef7cd696b8bcc3e0bed0e
Cédric Michel <c-e-d@live.fr>
2022-02-18 15:35:10 +0100

    background modal + chevron label 'trier par'

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,10 +1,17 @@
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
+    const modal = document.getElementById("background-modal");
 	modal.style.display = "block";
 }
 
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
+    const modal = document.getElementById("background-modal");
     modal.style.display = "none";
 }
 
+// var contactModal = document.forms.contact;
+// console.log(contactModal);
+
+// const submitModal = document.getElementById('submit-contact_button');
+// submitModal.addEventListener('click', () => {
+//     console.log(contactModal);
+// })
